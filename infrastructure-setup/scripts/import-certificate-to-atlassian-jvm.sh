8a87db980366e41802ee78da9302af8e98758dbe
will <will@github.com>
2019-06-29 21:32:16 +0800

    Change atlassian from 192.168.1.5 to 192.168.56.101

--- infrastructure-setup/scripts/import-certificate-to-atlassian-jvm.sh
+++ infrastructure-setup/scripts/import-certificate-to-atlassian-jvm.sh
@@ -10,7 +10,7 @@ if [ "$HOSTNAME" != "atlassian" ] ; then
 	exit
 fi
 
-openssl s_client -connect 192.168.1.5:8443 -showcerts < /dev/null 2>/dev/null| sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ${BASE_DIR}/openshift.crt
+openssl s_client -connect 192.168.56.101:8443 -showcerts < /dev/null 2>/dev/null| sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ${BASE_DIR}/openshift.crt
 yes yes | /etc/alternatives/jre/bin/keytool -delete -alias openshift -keystore /etc/alternatives/jre/lib/security/cacerts -storepass changeit > /dev/null
 yes yes | /etc/alternatives/jre/bin/keytool -delete -alias oc_router -keystore /etc/alternatives/jre/lib/security/cacerts -storepass changeit > /dev/null
 yes yes | /etc/alternatives/jre/bin/keytool -import -alias openshift -keystore /etc/alternatives/jre/lib/security/cacerts -storepass changeit -file ${BASE_DIR}/openshift.crt
